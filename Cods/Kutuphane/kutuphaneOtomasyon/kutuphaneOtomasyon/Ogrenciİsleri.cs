e4caac7dabe998c97261c26388f7e2e6e4207f81
esrakayhann <esrakayhan.2002@hotmail.com>
2022-05-12 22:08:33 +0300

    Create Ogrenciİsleri.cs

---
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/Ogrenciİsleri.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kutuphaneOtomasyon
+{
+    public partial class Ogrenciİsleri : Form
+    {
+        public Ogrenciİsleri()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
