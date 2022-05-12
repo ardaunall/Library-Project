eb8ea9c6e908213abf33eb2e3270d3c7469ded30
Arda Ünal <92482199+ardaunall@users.noreply.github.com>
2022-05-12 23:52:58 +0300

    Update GirisYap.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/GirisYap.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/GirisYap.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace kutuphaneOtomasyon
 {
     public partial class GirisYap : Form
     {
+        metod mtt = new metod();    
+
         public GirisYap()
         {
             InitializeComponent();
@@ -35,6 +38,51 @@ namespace kutuphaneOtomasyon
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void GirisYap_Shown(object sender, EventArgs e)
+        {
+            textBox2.Focus();
+        }
+
+        private void GirisYap_Load(object sender, EventArgs e)
+        {
+            #region admin üyelerini çektik
+            string ConStr = "SERVER=172.21.54.3;DATABASE=ARES;UID=ARES;PWD=Ares895900.";
+            using (var baglan = new MySqlConnection(ConStr))
+            {
+                using (var komut = new MySqlCommand("SELECT kullaniciadi FROM admin ORDER BY kullaniciadi ASC", baglan))
+                {
+                    try
+                    {
+                        komut.Connection.Open();
+                        MySqlDataReader dr = komut.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            kullanciadi.Items.Add(dr["kullaniciadi"].ToString());
+                        }
+                        kullanciadi.SelectedIndex = 0;
+                    }
+                    catch (Exception hata)
+                    {
+
+                        MessageBox.Show(hata.Message);
+                    }
+                }
+            }
+            #endregion
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(mtt.KullaniciKontrol(kullanciadi.SelectedItem.ToString(),textBox2.Text)==1)
+            {
+                MessageBox.Show("Giriş Başarılı", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Giriş Geçersiz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
