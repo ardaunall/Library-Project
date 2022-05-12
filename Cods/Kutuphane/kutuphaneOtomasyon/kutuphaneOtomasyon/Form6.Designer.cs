ad93c0508ab1e74f2a2466abaa5048d902052d3a
esrakayhann <esrakayhan.2002@hotmail.com>
2022-05-12 18:39:38 +0300

    Update Form6.Designer.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/Form6.Designer.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/Form6.Designer.cs
@@ -76,7 +76,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::kutuphaneOtomasyon.Properties.Resources.uu_logo;
-            this.pictureBox1.Location = new System.Drawing.Point(262, 9);
+            this.pictureBox1.Location = new System.Drawing.Point(262, 11);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(96, 94);
