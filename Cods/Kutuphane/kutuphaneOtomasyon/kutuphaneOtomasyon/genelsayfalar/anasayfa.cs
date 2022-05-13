fff1c8cc047c8f8f08a15d37c3a2b559fbf8978a
Arda Ünal <92482199+ardaunall@users.noreply.github.com>
2022-05-13 23:33:18 +0300

    Create anasayfa.cs

---
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/genelsayfalar/anasayfa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kutuphaneOtomasyon
+{
+    public partial class anasayfa : Form
+    {
+        public anasayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void cikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            GirisYap lgn = new GirisYap();
+            lgn.Show();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            
+        }
+
+        private void anasayfa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
