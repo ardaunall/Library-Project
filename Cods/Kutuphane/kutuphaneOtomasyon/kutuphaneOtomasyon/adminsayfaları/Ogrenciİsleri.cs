2d61114624aba9a140164d145b3023e712d198e1
Arda Ünal <92482199+ardaunall@users.noreply.github.com>
2022-05-13 23:33:04 +0300

    Create Ogrenciİsleri.cs

---
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/Ogrenciİsleri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kutuphaneOtomasyon
+{
+    public partial class Ogrenciİsleri : Form
+    {
+        public Ogrenciİsleri()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cikis_yap_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            GirisYap lgn = new GirisYap();
+            lgn.Show();
+        }
+
+    }
+}
