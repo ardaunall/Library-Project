bb937f1431bd7272cc167a008df9e01186eda60a
Arda Ünal <92482199+ardaunall@users.noreply.github.com>
2022-05-14 19:35:01 +0300

    Update studentKayit.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/ogrencisayfaları/studentKayit.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/ogrencisayfaları/studentKayit.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace kutuphaneOtomasyon
 {
-    public partial class Form5 : Form
+    public partial class studentKayit : Form
     {
-        public Form5()
+        public studentKayit()
         {
             InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection baglanti = new MySqlConnection("SERVER=172.21.54.3;DATABASE=ARES;UID=ARES;PWD=Ares895900.");
+            baglanti.Open();
+
+            MySqlCommand cmdekle = new MySqlCommand("insert into ogrenci (ogrenci_id,ad,soyad,bolum_ad,email) values (@ogrenci_id,@ad,@soyad,@bolum_ad,@email)", baglanti);
+            cmdekle.Parameters.AddWithValue("@ogrenci_id", txt_ogrno.Text);
+            cmdekle.Parameters.AddWithValue("@ad", txt_ad.Text);
+            cmdekle.Parameters.AddWithValue("@soyad", txt_soyad.Text);
+            cmdekle.Parameters.AddWithValue("@bolum_ad", cmb_bolum.Text);
+            cmdekle.Parameters.AddWithValue("@email", txt_email.Text);
+            cmdekle.ExecuteNonQuery();
+            baglanti.Close();
+
+            if (String.IsNullOrEmpty(txt_ogrno.Text) || String.IsNullOrEmpty(txt_ad.Text) || String.IsNullOrEmpty(txt_soyad.Text) || String.IsNullOrEmpty(cmb_bolum.Text) || String.IsNullOrEmpty(txt_email.Text))
+            {
+                MessageBox.Show("Boş Geçilemez");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Başarılı");
+
+            }
+
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            anasayfa don = new anasayfa();
+            don.Show();
         }
     }
 }
