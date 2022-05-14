d96e7af22f62b94f06e04bac18074470e7c5c6d5
esrakayhann <esrakayhan.2002@hotmail.com>
2022-05-14 15:32:25 +0300

    Create ceza.Designer.cs

---
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/ceza.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace kutuphaneOtomasyon.adminsayfaları
+{
+    partial class ceza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ceza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(86)))), ((int)(((byte)(131)))));
+            this.ClientSize = new System.Drawing.Size(1252, 525);
+            this.Name = "ceza";
+            this.Text = "ceza";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
