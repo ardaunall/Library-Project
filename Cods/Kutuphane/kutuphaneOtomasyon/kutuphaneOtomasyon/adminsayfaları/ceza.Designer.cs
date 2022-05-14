de9057d343700e698b34e4f85c4879dad0d03051
esrakayhann <esrakayhan.2002@hotmail.com>
2022-05-14 15:37:27 +0300

    Update ceza.Designer.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/ceza.Designer.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/ceza.Designer.cs
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(362, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(481, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ULUDAĞ ÜNİVERSİTESİ KÜTÜPHANESİ";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::kutuphaneOtomasyon.Properties.Resources.uu_logo;
+            this.pictureBox6.Location = new System.Drawing.Point(270, 11);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(85, 78);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
             // 
             // ceza
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(86)))), ((int)(((byte)(131)))));
-            this.ClientSize = new System.Drawing.Size(1252, 525);
+            this.ClientSize = new System.Drawing.Size(1079, 525);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
             this.Name = "ceza";
             this.Text = "ceza";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
