2c1b81633bec4b4f65be2f01c62099a50a93a2a3
Arda Ünal <92482199+ardaunall@users.noreply.github.com>
2022-05-14 18:10:58 +0300

    Update AdminKayit.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/AdminKayit.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/AdminKayit.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace kutuphaneOtomasyon
 {
@@ -19,7 +20,50 @@ namespace kutuphaneOtomasyon
 
         private void AdminKayit_Load(object sender, EventArgs e)
         {
+            MySqlConnection baglanti = new MySqlConnection("SERVER=172.21.54.3;DATABASE=ARES;UID=ARES;PWD=Ares895900.");
+            cmb_departman.Items.Add("Kütüphane Görevlisi");
+            cmb_departman.Items.Add("Öğrenci İşleri");
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region Personel Ekleme
+            MySqlConnection baglanti = new MySqlConnection("SERVER=172.21.54.3;DATABASE=ARES;UID=ARES;PWD=Ares895900.");
+            baglanti.Open();
+
+            MySqlCommand cmdekle = new MySqlCommand("insert into admin (id,ad,soyad,email,kullaniciadi,sifre,departman) values (@id,@ad,@soyad,@email,@kullaniciadi,@sifre,@departman)", baglanti);
+            cmdekle.Parameters.AddWithValue("@id", txt_id.Text);
+            cmdekle.Parameters.AddWithValue("@ad", txt_ad.Text);
+            cmdekle.Parameters.AddWithValue("@soyad", txt_soyad.Text);
+            cmdekle.Parameters.AddWithValue("@email", txt_email.Text);
+            cmdekle.Parameters.AddWithValue("@kullaniciadi", txt_kullaniciadi.Text);
+            cmdekle.Parameters.AddWithValue("@sifre", txt_sifre.Text);
+            cmdekle.Parameters.AddWithValue("@departman", cmb_departman.Text);
+            
+
+            if (String.IsNullOrEmpty(txt_id.Text) || String.IsNullOrEmpty(txt_ad.Text) || String.IsNullOrEmpty(txt_soyad.Text) || String.IsNullOrEmpty(txt_email.Text) || String.IsNullOrEmpty(txt_kullaniciadi.Text) || String.IsNullOrEmpty(txt_sifre.Text) || String.IsNullOrEmpty(cmb_departman.Text))
+            {
+                MessageBox.Show("Boş Geçilemez");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Başarılı");
+
+                GirisYap accc = new GirisYap();
+                this.Hide();
+                accc.Show();
+            }
+
+            cmdekle.ExecuteNonQuery();
+            baglanti.Close();
+
+            #endregion
+
+        }
+
+        private void cmb_departman_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
