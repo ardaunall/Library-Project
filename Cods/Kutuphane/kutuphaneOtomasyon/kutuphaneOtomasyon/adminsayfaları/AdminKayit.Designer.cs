0e5791059e457e1ec20563574837495a29e9a8aa
esrakayhann <esrakayhan.2002@hotmail.com>
2022-05-15 17:00:10 +0300

    Update AdminKayit.Designer.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/AdminKayit.Designer.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/adminsayfaları/AdminKayit.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmb_departman = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.txt_sifre = new System.Windows.Forms.TextBox();
@@ -44,9 +47,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.cmb_departman = new System.Windows.Forms.ComboBox();
-            this.txt_id = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -70,23 +70,50 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(286, 90);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(381, 111);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(341, 351);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(455, 432);
             this.groupBox1.TabIndex = 21;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "KAYIT SAYFASI";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(185, 119);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(156, 22);
+            this.txt_id.TabIndex = 24;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(17, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(159, 29);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Personel No:";
+            // 
+            // cmb_departman
+            // 
+            this.cmb_departman.FormattingEnabled = true;
+            this.cmb_departman.Location = new System.Drawing.Point(185, 282);
+            this.cmb_departman.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmb_departman.Name = "cmb_departman";
+            this.cmb_departman.Size = new System.Drawing.Size(156, 24);
+            this.cmb_departman.TabIndex = 22;
+            this.cmb_departman.SelectedIndexChanged += new System.EventHandler(this.cmb_departman_SelectedIndexChanged);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label8.Location = new System.Drawing.Point(22, 229);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(29, 282);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(114, 25);
+            this.label8.Size = new System.Drawing.Size(145, 29);
             this.label8.TabIndex = 20;
             this.label8.Text = "Departman:";
             // 
@@ -94,38 +121,36 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(79, 196);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(105, 241);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(58, 25);
+            this.label7.Size = new System.Drawing.Size(72, 29);
             this.label7.TabIndex = 19;
             this.label7.Text = "Şifre:";
             // 
             // txt_sifre
             // 
-            this.txt_sifre.Location = new System.Drawing.Point(139, 196);
-            this.txt_sifre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_sifre.Location = new System.Drawing.Point(185, 241);
+            this.txt_sifre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_sifre.Name = "txt_sifre";
-            this.txt_sifre.Size = new System.Drawing.Size(118, 20);
+            this.txt_sifre.Size = new System.Drawing.Size(156, 22);
             this.txt_sifre.TabIndex = 18;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(23, 162);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(31, 199);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.Size = new System.Drawing.Size(0, 29);
             this.label6.TabIndex = 17;
             // 
             // button1
             // 
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(139, 289);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(185, 356);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 27);
+            this.button1.Size = new System.Drawing.Size(157, 33);
             this.button1.TabIndex = 16;
             this.button1.Text = "Kaydet";
             this.button1.UseVisualStyleBackColor = true;
@@ -133,44 +158,43 @@
             // 
             // txt_kullaniciadi
             // 
-            this.txt_kullaniciadi.Location = new System.Drawing.Point(139, 163);
-            this.txt_kullaniciadi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_kullaniciadi.Location = new System.Drawing.Point(185, 201);
+            this.txt_kullaniciadi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_kullaniciadi.Name = "txt_kullaniciadi";
-            this.txt_kullaniciadi.Size = new System.Drawing.Size(118, 20);
+            this.txt_kullaniciadi.Size = new System.Drawing.Size(156, 22);
             this.txt_kullaniciadi.TabIndex = 4;
             // 
             // txt_email
             // 
-            this.txt_email.Location = new System.Drawing.Point(139, 130);
-            this.txt_email.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_email.Location = new System.Drawing.Point(185, 160);
+            this.txt_email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_email.Name = "txt_email";
-            this.txt_email.Size = new System.Drawing.Size(118, 20);
+            this.txt_email.Size = new System.Drawing.Size(156, 22);
             this.txt_email.TabIndex = 3;
             // 
             // txt_soyad
             // 
-            this.txt_soyad.Location = new System.Drawing.Point(139, 64);
-            this.txt_soyad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_soyad.Location = new System.Drawing.Point(185, 79);
+            this.txt_soyad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_soyad.Name = "txt_soyad";
-            this.txt_soyad.Size = new System.Drawing.Size(118, 20);
+            this.txt_soyad.Size = new System.Drawing.Size(156, 22);
             this.txt_soyad.TabIndex = 2;
             // 
             // txt_ad
             // 
-            this.txt_ad.Location = new System.Drawing.Point(139, 31);
-            this.txt_ad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_ad.Location = new System.Drawing.Point(185, 38);
+            this.txt_ad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_ad.Name = "txt_ad";
-            this.txt_ad.Size = new System.Drawing.Size(118, 20);
+            this.txt_ad.Size = new System.Drawing.Size(156, 22);
             this.txt_ad.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(82, 31);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(109, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.Size = new System.Drawing.Size(67, 29);
             this.label2.TabIndex = 0;
             this.label2.Text = "İsim:";
             // 
@@ -178,10 +202,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(49, 64);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(65, 79);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 25);
+            this.label3.Size = new System.Drawing.Size(111, 29);
             this.label3.TabIndex = 0;
             this.label3.Text = "Soyisim:";
             // 
@@ -189,10 +212,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(63, 130);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(84, 160);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 25);
+            this.label4.Size = new System.Drawing.Size(93, 29);
             this.label4.TabIndex = 0;
             this.label4.Text = "E-mail:";
             // 
@@ -200,10 +222,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(13, 163);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(17, 201);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(125, 25);
+            this.label5.Size = new System.Drawing.Size(160, 29);
             this.label5.TabIndex = 0;
             this.label5.Text = "Kullanıcı Adı:";
             // 
@@ -213,62 +234,33 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(226, 24);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(301, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(538, 25);
+            this.label1.Size = new System.Drawing.Size(671, 29);
             this.label1.TabIndex = 20;
             this.label1.Text = "ULUDAĞ ÜNİVERSİTESİ KÜTÜPHANESİ KAYIT SAYFASI";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::kutuphaneOtomasyon.Properties.Resources.uu_logo;
-            this.pictureBox1.Location = new System.Drawing.Point(159, 5);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(212, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(62, 62);
+            this.pictureBox1.Size = new System.Drawing.Size(83, 76);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 19;
             this.pictureBox1.TabStop = false;
             // 
-            // cmb_departman
-            // 
-            this.cmb_departman.FormattingEnabled = true;
-            this.cmb_departman.Location = new System.Drawing.Point(139, 229);
-            this.cmb_departman.Name = "cmb_departman";
-            this.cmb_departman.Size = new System.Drawing.Size(118, 21);
-            this.cmb_departman.TabIndex = 22;
-            this.cmb_departman.SelectedIndexChanged += new System.EventHandler(this.cmb_departman_SelectedIndexChanged);
-            // 
-            // txt_id
-            // 
-            this.txt_id.Location = new System.Drawing.Point(139, 97);
-            this.txt_id.Margin = new System.Windows.Forms.Padding(2);
-            this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(118, 20);
-            this.txt_id.TabIndex = 24;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label9.Location = new System.Drawing.Point(13, 97);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(125, 25);
-            this.label9.TabIndex = 23;
-            this.label9.Text = "Personel No:";
-            // 
             // AdminKayit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(86)))), ((int)(((byte)(131)))));
-            this.ClientSize = new System.Drawing.Size(848, 530);
+            this.ClientSize = new System.Drawing.Size(1189, 568);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AdminKayit";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.AdminKayit_Load);
