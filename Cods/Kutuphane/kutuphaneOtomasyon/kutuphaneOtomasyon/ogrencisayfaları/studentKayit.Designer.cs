04a6e483ad48ea35ac86a6f019b2c929aeca30ea
Arda Ünal <92482199+ardaunall@users.noreply.github.com>
2022-05-15 16:35:58 +0300

    Update studentKayit.Designer.cs

--- Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/ogrencisayfaları/studentKayit.Designer.cs
+++ Cods/Kutuphane/kutuphaneOtomasyon/kutuphaneOtomasyon/ogrencisayfaları/studentKayit.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(studentKayit));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmb_bolum = new System.Windows.Forms.ComboBox();
             this.txt_ogrno = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -42,11 +43,15 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.cmb_bolum = new System.Windows.Forms.ComboBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_listele = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -63,19 +68,30 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(309, 102);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(90, 155);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(301, 267);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "KAYIT SAYFASI";
             // 
+            // cmb_bolum
+            // 
+            this.cmb_bolum.FormattingEnabled = true;
+            this.cmb_bolum.Items.AddRange(new object[] {
+            "Yönetim Bilişim Sistemleri",
+            "İşletme"});
+            this.cmb_bolum.Location = new System.Drawing.Point(145, 101);
+            this.cmb_bolum.Name = "cmb_bolum";
+            this.cmb_bolum.Size = new System.Drawing.Size(121, 21);
+            this.cmb_bolum.TabIndex = 19;
+            // 
             // txt_ogrno
             // 
             this.txt_ogrno.Location = new System.Drawing.Point(145, 168);
-            this.txt_ogrno.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_ogrno.Margin = new System.Windows.Forms.Padding(2);
             this.txt_ogrno.Name = "txt_ogrno";
             this.txt_ogrno.Size = new System.Drawing.Size(121, 20);
             this.txt_ogrno.TabIndex = 18;
@@ -95,7 +111,7 @@
             // 
             this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Location = new System.Drawing.Point(158, 213);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 27);
             this.button1.TabIndex = 16;
@@ -106,7 +122,7 @@
             // txt_email
             // 
             this.txt_email.Location = new System.Drawing.Point(145, 135);
-            this.txt_email.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_email.Margin = new System.Windows.Forms.Padding(2);
             this.txt_email.Name = "txt_email";
             this.txt_email.Size = new System.Drawing.Size(121, 20);
             this.txt_email.TabIndex = 4;
@@ -114,7 +130,7 @@
             // txt_soyad
             // 
             this.txt_soyad.Location = new System.Drawing.Point(145, 68);
-            this.txt_soyad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_soyad.Margin = new System.Windows.Forms.Padding(2);
             this.txt_soyad.Name = "txt_soyad";
             this.txt_soyad.Size = new System.Drawing.Size(121, 20);
             this.txt_soyad.TabIndex = 2;
@@ -122,7 +138,7 @@
             // txt_ad
             // 
             this.txt_ad.Location = new System.Drawing.Point(145, 35);
-            this.txt_ad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_ad.Margin = new System.Windows.Forms.Padding(2);
             this.txt_ad.Name = "txt_ad";
             this.txt_ad.Size = new System.Drawing.Size(121, 20);
             this.txt_ad.TabIndex = 1;
@@ -177,7 +193,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(182, 44);
+            this.label1.Location = new System.Drawing.Point(500, 34);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(634, 25);
@@ -187,53 +203,88 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::kutuphaneOtomasyon.Properties.Resources.uu_logo;
-            this.pictureBox1.Location = new System.Drawing.Point(116, 29);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(434, 19);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(62, 62);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
             // 
-            // cmb_bolum
-            // 
-            this.cmb_bolum.FormattingEnabled = true;
-            this.cmb_bolum.Items.AddRange(new object[] {
-            "Yönetim Bilişim Sistemleri",
-            "İşletme"});
-            this.cmb_bolum.Location = new System.Drawing.Point(145, 101);
-            this.cmb_bolum.Name = "cmb_bolum";
-            this.cmb_bolum.Size = new System.Drawing.Size(121, 21);
-            this.cmb_bolum.TabIndex = 19;
-            // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(25, 44);
+            this.pictureBox2.Location = new System.Drawing.Point(57, 34);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(55, 59);
             this.pictureBox2.TabIndex = 19;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(681, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(680, 252);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(538, 170);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 21;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(417, 170);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Öğrenci No:";
+            // 
+            // btn_listele
+            // 
+            this.btn_listele.ForeColor = System.Drawing.Color.Black;
+            this.btn_listele.Location = new System.Drawing.Point(538, 216);
+            this.btn_listele.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_listele.Name = "btn_listele";
+            this.btn_listele.Size = new System.Drawing.Size(121, 30);
+            this.btn_listele.TabIndex = 20;
+            this.btn_listele.Text = "Listele";
+            this.btn_listele.UseVisualStyleBackColor = true;
+            this.btn_listele.Click += new System.EventHandler(this.btn_listele_Click);
+            // 
             // studentKayit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(86)))), ((int)(((byte)(131)))));
-            this.ClientSize = new System.Drawing.Size(888, 439);
+            this.ClientSize = new System.Drawing.Size(1574, 660);
+            this.Controls.Add(this.btn_listele);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "studentKayit";
-            this.Text = "Form5";
+            this.Text = " ";
             this.Load += new System.EventHandler(this.Form5_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -256,5 +307,9 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ComboBox cmb_bolum;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn_listele;
     }
 }
